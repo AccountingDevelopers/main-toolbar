0cdc83c4442f97810be39541d105bdb42da7c2f5
unknown <millerkirillmail@gmail.com>
2023-08-02 01:12:05 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { AccSystemService } from 'ng-accounting';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'main-toolbar';
+    constructor(private readonly accSystemService: AccSystemService) { }
+    currentUser!: any
+
+    ngOnInit(): void {
+        this.currentUser = this.accSystemService.currentUser
+        console.log(this.currentUser);
+
+    }
+
+    getAvatar() {
+        return this.currentUser.name.first.split('').pop()
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgAccountingModule } from 'ng-accounting'
+import { ButtonModule } from 'primeng/button'
+import { AvatarModule } from 'primeng/avatar'
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgAccountingModule.forRoot({
+            api: {
+                key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGFlZGY2YmRmMWRhOTkwZWI0MmI1YjQiLCJjb21wYW55SWQiOiI2NGFmZmQ4ZDhlYjFkMzJhODBlM2I0YWIiLCJ3b3Jrc3BhY2VOYW1lIjoiam91cm5leSIsImlhdCI6MTY5MDU2MjAxMSwiZXhwIjoxNzE2NDgyMDExfQ.BRZ-3OIJ4qMpoH0CbEFr05mHfQKV4r1nZlKOuWiXv8U',
+                url: {
+                    client: 'http://localhost:4200',
+                    server: isDevMode() ? 'http://localhost:80/api/v1' : 'http://localhost:80/api/v1'
+                }
+            }
+        }),
+        ButtonModule,
+        AvatarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
